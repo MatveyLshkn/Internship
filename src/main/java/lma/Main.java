ad4338e919d0242f2028f12f4178aa74a1deb809
MatveyLshkn <115181274+MatveyLshkn@users.noreply.github.com>
2024-07-03 13:04:14 +0300

    Update Main.java

--- src/main/java/lma/Main.java
+++ src/main/java/lma/Main.java
@@ -3,5 +3,9 @@ package lma;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a =0;
+        int b = 1;
+        int sum = a+b;
+        
     }
 }
