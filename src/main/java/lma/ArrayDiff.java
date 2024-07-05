026de01ad84b6e069670ab7b0fbabf94dc2e4e3b
MatveyLshkn <lashkinmatveyfw@gmail.com>
2024-07-05 12:10:44 +0300

    ArrayDiff solution

---
+++ src/main/java/lma/ArrayDiff.java
@@ -0,0 +1,28 @@
+package lma;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+//https://www.codewars.com/kata/523f5d21c841566fde000009
+public class ArrayDiff {
+    public static int[] arrayDiff(int[] a, int[] b) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < a.length; i++) {
+            set.add(a[i]);
+        }
+        for (int i = 0; i < b.length; i++) {
+            if (set.contains(b[i])) {
+                set.remove(b[i]);
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < a.length; i++) {
+            if(set.contains(a[i])) {
+                ans.add(a[i]);
+            }
+        }
+        return ans.stream().mapToInt(i -> i).toArray();
+    }
+
+
+}
