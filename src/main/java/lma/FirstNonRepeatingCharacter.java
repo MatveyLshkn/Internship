636d16798885c31eef24e6b2957b4afb8c61ff2d
MatveyLshkn <lashkinmatveyfw@gmail.com>
2024-07-09 09:50:21 +0300

    FirstNonRepeatingCharacter solution

---
+++ src/main/java/lma/FirstNonRepeatingCharacter.java
@@ -0,0 +1,21 @@
+package lma;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//https://www.codewars.com/kata/52bc74d4ac05d0945d00054e
+public class FirstNonRepeatingCharacter {
+    public static String firstNonRepeatingLetter(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toLowerCase().toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ((Character.isUpperCase(c)
+                 && map.get(Character.toLowerCase(c)) == 1)
+                || (map.containsKey(c) && map.get(c) == 1)) return c + "";
+        }
+        return "";
+    }
+}
