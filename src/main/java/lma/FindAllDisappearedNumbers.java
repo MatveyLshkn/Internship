5d6ec2f37f7cefa175c9f726143cec0907e890c5
MatveyLshkn <lashkinmatveyfw@gmail.com>
2024-07-11 10:59:42 +0300

    Made FindAllDisappearedNumbers solution more readable

--- src/main/java/lma/FindAllDisappearedNumbers.java
+++ src/main/java/lma/FindAllDisappearedNumbers.java
@@ -8,14 +8,18 @@ public class FindAllDisappearedNumbers {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             int pos = nums[i];
-            if (pos < 0)
+            if (pos < 0) {
                 pos = -pos;
-            if (nums[pos - 1] > 0)
+            }
+            if (nums[pos - 1] > 0) {
                 nums[pos - 1] = -nums[pos - 1];
+            }
         }
         List<Integer> ans = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) ans.add(i + 1);
+            if (nums[i] > 0) {
+                ans.add(i + 1);
+            }
         }
         return ans;
     }
