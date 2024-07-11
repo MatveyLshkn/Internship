6bc5a1c21906772b12a78f97bf0d0b138b9f8335
MatveyLshkn <lashkinmatveyfw@gmail.com>
2024-07-11 14:35:49 +0300

    Made SumOfDigits solution more readable

--- src/main/java/lma/SumOfDigits.java
+++ src/main/java/lma/SumOfDigits.java
@@ -7,7 +7,7 @@ public class SumOfDigits {
         int sum = 0;
         while (n != 0) {
             int remains = n % 10;
-            n /= 10;
+            n = n / 10;
             sum += remains;
         }
         return digital_root(sum);
