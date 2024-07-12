2c3ed9bc357484e4a96658d73113095687e5184a
MatveyLshkn <lashkinmatveyfw@gmail.com>
2024-07-12 14:49:09 +0300

    Added BestTimeToBuySellStock solution

---
+++ src/main/java/lma/BestTimeToBuySellStock.java
@@ -0,0 +1,14 @@
+package lma;
+
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+public class BestTimeToBuySellStock {
+    public int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for(int i = 1; i < prices.length; i++){
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+        return maxProfit;
+    }
+}
