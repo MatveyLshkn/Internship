c245996829e18c9b70ee80ebfaf36cd682b81278
MatveyLshkn <lashkinmatveyfw@gmail.com>
2024-07-17 09:24:46 +0300

    Added SameTree solution

---
+++ src/main/java/lma/SameTree.java
@@ -0,0 +1,33 @@
+package lma;
+
+import javax.swing.tree.TreeNode;
+
+//https://leetcode.com/problems/same-tree/
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        return p.val == q.val &&
+               isSameTree(p.left, q.left) &&
+               isSameTree(p.right, q.right);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
