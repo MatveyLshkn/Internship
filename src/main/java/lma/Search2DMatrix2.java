081bfea69d27c65c94ffa33bafb0d43b9c8f2d05
MatveyLshkn <lashkinmatveyfw@gmail.com>
2024-07-18 09:30:02 +0300

    Added Search2DMatrix2 solution

---
+++ src/main/java/lma/Search2DMatrix2.java
@@ -0,0 +1,19 @@
+package lma;
+
+//https://leetcode.com/problems/search-a-2d-matrix-ii/
+public class Search2DMatrix2 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0;
+        int j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] > target) {
+                j--;
+            } else if (matrix[i][j] < target) {
+                i++;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
