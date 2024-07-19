bd6e80d63910958d58d785b62be1fad475cedefa
MatveyLshkn <lashkinmatveyfw@gmail.com>
2024-07-19 17:10:58 +0300

    Refactor

--- src/main/java/lma/IntervalListIntersections.java
+++ src/main/java/lma/IntervalListIntersections.java
@@ -7,18 +7,21 @@ import java.util.List;
 public class IntervalListIntersections {
     public static int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
         List<int[]> ans = new ArrayList<>();
-        int first = 0;
-        int second = 0;
-        while (first < firstList.length && second < secondList.length) {
-            if (secondList[second][0] <= firstList[first][1] && firstList[first][0] <= secondList[second][1])  {
+        int firstIndex = 0;
+        int secondIndex = 0;
+        while (firstIndex < firstList.length && secondIndex < secondList.length) {
+            if (secondList[secondIndex][0] <= firstList[firstIndex][1] &&
+                firstList[firstIndex][0] <= secondList[secondIndex][1]) {
                 int[] pair = new int[2];
-                pair[0] = Math.max(firstList[first][0], secondList[second][0]);
-                pair[1] = Math.min(firstList[first][1], secondList[second][1]);
+                pair[0] = Math.max(firstList[firstIndex][0], secondList[secondIndex][0]);
+                pair[1] = Math.min(firstList[firstIndex][1], secondList[secondIndex][1]);
                 ans.add(pair);
             }
-            if (secondList[second][1] > firstList[first][1]) {
-                first++;
-            } else second++;
+            if (secondList[secondIndex][1] > firstList[firstIndex][1]) {
+                firstIndex++;
+            } else {
+                secondIndex++;
+            }
         }
         return ans.toArray(new int[ans.size()][]);
     }
