23902574735fb0d79c73e44538b59bbe15946675
MatveyLshkn <lashkinmatveyfw@gmail.com>
2024-07-30 16:48:25 +0300

    Added MinimumDepthOfBinaryTree solution

---
+++ src/main/java/lma/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,38 @@
+package lma;
+
+import javax.swing.tree.TreeNode;
+
+//https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+public class MinimumDepthOfBinaryTree {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null) {
+            return 1 + minDepth(root.right);
+        }
+        if (root.right == null) {
+            return 1 + minDepth(root.left);
+        }
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
