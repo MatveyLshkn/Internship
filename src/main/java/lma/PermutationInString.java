dcd20fd619f1acdd6a5319f183b6e584736f6784
MatveyLshkn <lashkinmatveyfw@gmail.com>
2024-08-23 10:38:16 +0300

    removed static keyword

--- src/main/java/lma/PermutationInString.java
+++ src/main/java/lma/PermutationInString.java
@@ -2,7 +2,8 @@ package lma;
 
 //https://leetcode.com/problems/permutation-in-string/description/
 public class PermutationInString {
-    public static boolean checkInclusion(String s1, String s2) {
+
+    public boolean checkInclusion(String s1, String s2) {
 
         int map[] = new int[26];
         for (char ch : s1.toCharArray()) {
@@ -19,19 +20,18 @@ public class PermutationInString {
                 remains--;
             }
 
-            map[s2.charAt(right) - 'a']--;
-            right++;
-
             if (remains == 0) {
                 return true;
             }
+
+            map[s2.charAt(right) - 'a']--;
+            right++;
 
             if (right - left == s1.length()) {
 
                 if (map[s2.charAt(left) - 'a'] >= 0) {
                     remains++;
                 }
-
                 map[s2.charAt(left) - 'a']++;
                 left++;
 
